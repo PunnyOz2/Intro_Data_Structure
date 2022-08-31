095555a7c2069c838b8efbf2af2fe901796acd84
Tawatwee Aiemarreerat <77015953+PunnyOz2@users.noreply.github.com>
2022-08-31 04:03:42 +0000

    change student.h

--- d57_q1_postfix/student.h
+++ d57_q1_postfix/student.h
@@ -7,83 +7,33 @@ using namespace std;
 int eval_postfix(vector<pair<int, int>> v)
 {
   stack<int> s;
-  stack<int> o;
-  vector<int> priority = {3, 3, 5, 5};
-  int temp, tempOfNum, tempOfNum2;
+  int tempOfNum, tempOfNum2;
   for (auto &x : v) {
     if (x.first == 1) s.push(x.second);
     else {
+      tempOfNum = s.top();
+      s.pop();
+      tempOfNum2 = s.top();
+      s.pop();
       switch (x.second)
       {
       case 0:
-        tempOfNum = s.top();
-        s.pop();
-        tempOfNum2 = s.top();
-        s.pop();
         s.push(tempOfNum + tempOfNum2);
         break;
       case 1:
-        tempOfNum = s.top();
-        s.pop();
-        tempOfNum2 = s.top();
-        s.pop();
         s.push(tempOfNum2 - tempOfNum);
         break;
       case 2:
-        tempOfNum = s.top();
-        s.pop();
-        tempOfNum2 = s.top();
-        s.pop();
         s.push(tempOfNum * tempOfNum2);
         break;
       case 3:
-        tempOfNum = s.top();
-        s.pop();
-        tempOfNum2 = s.top();
-        s.pop();
         s.push(tempOfNum2 / tempOfNum);
         break;
       default:
+        s.push(tempOfNum2);
+        s.push(tempOfNum);
         break;
       }
-    }
-  }
-  while (!o.empty())
-  {
-    temp = o.top();
-    o.pop();
-    switch (temp)
-    {
-    case 0:
-      tempOfNum = s.top();
-      s.pop();
-      tempOfNum2 = s.top();
-      s.pop();
-      s.push(tempOfNum + tempOfNum2);
-      break;
-    case 1:
-      tempOfNum = s.top();
-      s.pop();
-      tempOfNum2 = s.top();
-      s.pop();
-      s.push(tempOfNum2 - tempOfNum);
-      break;
-    case 2:
-      tempOfNum = s.top();
-      s.pop();
-      tempOfNum2 = s.top();
-      s.pop();
-      s.push(tempOfNum * tempOfNum2);
-      break;
-    case 3:
-      tempOfNum = s.top();
-      s.pop();
-      tempOfNum2 = s.top();
-      s.pop();
-      s.push(tempOfNum2 / tempOfNum);
-      break;
-    default:
-      break;
     }
   }
   tempOfNum = s.top();
